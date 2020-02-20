d0437a8843458038d475fd230b73f64ae13b6bef
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-02-20 14:31:41 -0300

    Alteração do projeto

--- ExercicioMod9Pratica/Program.cs
+++ ExercicioMod9Pratica/Program.cs
@@ -19,6 +19,22 @@ namespace ExercicioMod9Pratica
             string email = Console.ReadLine();
             Console.Write("Birth date (DD/MM/YYYY): ");
             DateTime birthdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: "+DateTime.Now);
+            Console.Write("Status: 0 - Pending Payment/ 1 - Processing / 2 - Shipped / 3 - Delivered  ");
+            string status = Console.ReadLine();
+            OrderStatus order = (OrderStatus)Enum.Parse(typeof(OrderStatus), status);
+            OrderItem item = new OrderItem();
+            Client cliente = new Client(name, email, birthdate);
+
+            Console.Write("How many itens to this order?: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+
+
+            }
+
 
 
 
