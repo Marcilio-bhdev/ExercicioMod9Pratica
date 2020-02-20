bae59af71c3698d4730f1ef59fefdf3250ec5c6b
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-02-20 14:55:10 -0300

    Nova alteração do produto

--- ExercicioMod9Pratica/Program.cs
+++ ExercicioMod9Pratica/Program.cs
@@ -12,6 +12,7 @@ namespace ExercicioMod9Pratica
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Enter Cliente Data: ");
             Console.Write("Name: ");
             string name = Console.ReadLine();
@@ -25,15 +26,29 @@ namespace ExercicioMod9Pratica
             OrderStatus order = (OrderStatus)Enum.Parse(typeof(OrderStatus), status);
             OrderItem item = new OrderItem();
             Client cliente = new Client(name, email, birthdate);
+           
 
             Console.Write("How many itens to this order?: ");
             int n = int.Parse(Console.ReadLine());
             for (int i = 1; i < n; i++)
             {
                 Console.WriteLine($"Enter #{i} item data:");
-
-
+                Console.Write("Product Name: ");
+                string namepro = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double pricePro = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantityPro = int.Parse(Console.ReadLine());
+                Product pro = new Product(namepro, pricePro);
+                OrderItem lista = new OrderItem(quantityPro, pricePro);
+                
             }
+
+            Console.WriteLine("Order Sumary:");
+            Console.WriteLine("Order moment: "+DateTime.Now);
+            Console.WriteLine("Order Status: "+status );
+            Console.WriteLine("Client: "+ cliente.Name+"("+cliente.BirthDate+")"+" - "+cliente.Email);
+
 
 
 
